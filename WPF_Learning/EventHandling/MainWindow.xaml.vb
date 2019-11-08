b6d74032bd9f49c8d8cb8a7bb7a81a17e498f0d0
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2019-11-08 06:29:34 -0800

    Added event handling for form click

--- WPF_Learning/EventHandling/MainWindow.xaml.vb
+++ WPF_Learning/EventHandling/MainWindow.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Class MainWindow
-
+    Private Sub pnlEventHandling_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        MessageBox.Show("You clicked me at " + e.GetPosition(Me).ToString())
+    End Sub
 End Class
